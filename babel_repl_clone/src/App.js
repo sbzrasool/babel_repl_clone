59592656a3f3d2816d398e401fae3190c9fbdb2e
sbzrasool <sbzrasool@gmail.com>
2020-12-29 21:10:59 +0530

    Completed Code updates

--- babel_repl_clone/src/App.js
+++ babel_repl_clone/src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
+
+import _ from 'lodash';
+import * as babel from '@babel/standalone';
+
+import Input from './components/Input';
+import Output from './components/Output';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+  const ref = useRef();
+
+  // start --------- Debounce logic for typed input 
+  useEffect(() => {
+    ref.current = _.debounce(generateResult, 1000);
+  }, []);
+  // end -----------
+
+  // start --------- Save the input code 
+  useEffect(() => {
+    try{
+      setInput(JSON.parse(localStorage.getItem('babel_code')));
+    } catch(e){
+      setInput('');
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('babel_code', JSON.stringify(input));
+  }, [input]);
+  // end ----------
+
+
+  function generateResult(value){
+    try{
+      const result = babel.transform(value, {
+        presets : ['env', 'react'],
+        filename : '/App.js'
+      }).code;
+
+      setOutput(result);
+      setErrorMsg('');
+    } catch (e) {
+      setErrorMsg(e.message);
+    }
+  }
+
+  function handleInputChange(event){
+      const value = event.target.value;
+
+      setInput(value);
+      
+      ref.current(value)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Babel Transpiler</h1>
+      <Input 
+        input = {input ? input : ''}
+        handleInputChange = {handleInputChange}
+        errorMsg = {errorMsg}
+      />
+      <Output 
+        output = {output}
+        hasError = {!_.isEmpty(errorMsg)}
+      />
     </div>
   );
-}
+};
 
 export default App;
---
+++ babel_repl_clone/src/components/Input.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Error from './Error';
+
+const Input = ( { input, handleInputChange, errorMsg } ) => {
+    return(
+        <div className="input">
+            <textarea 
+                className="input-box"
+                placeholder="Write code here"
+                value={input}
+                onChange={handleInputChange}
+            />
+            {
+                errorMsg && (
+                    <div className="error">
+                        <Error errorMsg={errorMsg}/>
+                    </div>
+                )
+            }
+        </div>
+    );
+}
+
+export default Input;
